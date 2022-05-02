e13083615a1439679d113bc0ed6143b638883391
Aditya Mishra <mishraaaditya29@gmail.com>
2022-05-02 11:14:51 +0530

    git commit

---
+++ 1. C++ Basics/Datatypes.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// char, int, bool, float, doubles are basic datatypes
+	char c = 'a';
+	int a = 3;
+	double b = 3.5;//used to store floating-point values up to 15 digits
+	a = 4.5;
+	bool d = true;
+	cout<<c<<" "<<a<<" "<<b<<" "<<d<<endl;
+}
