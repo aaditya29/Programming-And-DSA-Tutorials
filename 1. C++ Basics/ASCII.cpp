759ab4d272d2c8e66d5d4c23588760324cc38b5e
Aditya Mishra <mishraaaditya29@gmail.com>
2022-05-02 12:02:45 +0530

    git commit

---
+++ 1. C++ Basics/ASCII.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	char c = 'a';
+	cout<< (int) c<<endl;//typecasting to know ASCII value of 'a'
+	char b = 'c';
+	cout<< (int) b<<endl;
+	cout<<c-b<<endl;
+}
