da86c219f6e7790b85df1f5d86e3124478beb2ae
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-23 19:33:06 +0530

    git commit

--- 3. STL/Intro2.cpp
+++ 3. STL/Intro2.cpp
@@ -2,7 +2,15 @@
 
 #include <iostream>
 #include <vector>
+using namespace std;
 
+template <typename T>
+T square(T x)
+{
+    return x * x;
+}
 int main()
 {
+    cout << square<int>(5) << endl;
+    cout << square<int>(3.5) << endl;
 }
