b2699690549aa60132eb832d4ac7ab440ab3b690
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-16 16:03:40 +0530

    templates working added

--- 4. STL/Templates.cpp
+++ 4. STL/Templates.cpp
@@ -1,92 +1,39 @@
 
 #include <iostream>
-#include <cstdlib>
-#include <csignal>
-#include <csetjmp>
-#include <cstdarg>
-#include <typeinfo>
-#include <typeindex>
-#include <type_traits>
-#include <bitset>
-#include <functional>
-#include <utility>
-#include <ctime>
-#include <chrono>
-#include <cstddef>
-#include <initializer_list>
-#include <tuple>
-#include <new>
-#include <memory>
-#include <scoped_allocator>
-#include <climits>
-#include <cfloat>
-#include <cstdint>
-#include <cinttypes>
-#include <limits>
-#include <exception>
-#include <stdexcept>
-#include <cassert>
-#include <system_error>
-#include <cerrno>
-#include <cctype>
-#include <cwctype>
-#include <cstring>
-#include <cwchar>
-#include <string>
-#include <array>
-#include <vector>
-#include <deque>
-#include <list>
-#include <forward_list>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <iterator>
-#include <cmath>
-#include <complex>
-#include <valarray>
-#include <random>
-#include <numeric>
-#include <ratio>
-#include <cfenv>
-#include <iosfwd>
-#include <ios>
-#include <istream>
-#include <ostream>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <strstream>
-#include <iomanip>
-#include <streambuf>
-#include <cstdio>
-#include <locale>
-#include <clocale>
-#include <codecvt>
-#include <regex>
-#include <atomic>
-#include <thread>
-#include <mutex>
-#include <future>
-#include <condition_variable>
-#include <ciso646>
-#include <ccomplex>
-#include <ctgmath>
-#include <cstdbool>
+
 using namespace std;
 
 // Templates introduction
 
+/*
+
+Non conevnient way
 int square(int x)
 {
+    return x * x;
+}
+
+double square(double x){
     return x * x;
 }
 
 int main()
 {
     cout << square(5) << endl;
+    cout << square(5.5) << endl;
+}
+*/
+
+// using template
+
+template <typename T>
+T square(T x)
+{
+    return x * x;
+}
+
+int main()
+{
+    cout << square<int>(5) << endl; // telling square datatype is of int
+    cout << square(5.5) << endl;    // telling square datatype is of flo
 }
