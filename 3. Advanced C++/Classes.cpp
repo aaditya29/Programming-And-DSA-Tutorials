6ed094721fe4b25c7e2d203f3e9be075f2a569e5
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-19 11:17:20 +0530

    git commit

--- 3. Advanced C++/Classes.cpp
+++ 3. Advanced C++/Classes.cpp
@@ -3,3 +3,23 @@
 #include <iostream>
 
 using namespace std;
+
+class Player
+{
+public:
+    int x, y;
+    int speed;
+
+    void Move(int x, int y)
+    {
+        x += x * speed;
+        y += y * speed;
+    }
+};
+
+int main()
+{
+    Player player;
+    player.Move(1, -1);
+    cin.get();
+}
