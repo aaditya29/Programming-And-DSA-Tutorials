d85e6337cd0ec4236ffc3e30f5612c00581aacc5
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-19 19:26:47 +0530

    git commit

--- 3. Advanced C++/Classes2.cpp
+++ 3. Advanced C++/Classes2.cpp
@@ -6,6 +6,24 @@ using namespace std;
 
 class Log
 {
+public:
+    // for variables
+    const int LogLevelError = 0;
+    const int LogLevelWarning = 1;
+    const int LogLevelInfo = 2;
+
+private:
+    int m_LogLevel = LogLevelInfo; // private member
+
+public:
+    void SetLevel(int level)
+    {
+        m_LogLevel = level;
+    }
+
+    void Warn(const char *message)
+    {
+    }
 };
 
 int main()
@@ -14,5 +32,6 @@ int main()
     Log log;
     // Setting log level
     log.SetLevel(LogLevelWarning);
+    log.Warn("You are warned");
     cin.get();
 }
