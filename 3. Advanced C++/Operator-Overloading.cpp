fc9cf4095b8fb1a9f872210cf6d132ba52b05b03
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-23 18:10:44 +0530

    git commit

--- 3. Advanced C++/Operator-Overloading.cpp
+++ 3. Advanced C++/Operator-Overloading.cpp
@@ -5,12 +5,30 @@
 
 using namespace std;
 
-struct vector2
+struct Vector2
 {
     float x, y;
+
+    // Defining constructor
+    Vector2(float x, float y)
+        : x(x), y(y) {}
+
+    Vector2 Add(const Vector2 &other) const
+    {
+        return Vector2(x + other.x, y + other.y);
+    }
+
+    Vector2 Multiply(const Vector2 &other) const
+    {
+        return Vector2(x * other.x, y * other.y);
+    }
 };
 
 int main()
 {
-    cin.get();
+    Vector2 position(4, 5);
+    Vector2 speed(6, 4);
+    Vector2 powerup(5, 4);
+
+        cin.get();
 }
