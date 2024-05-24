8224a1edc0876be88cfc37cef258a5885fbc71d8
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-24 18:11:34 +0530

    git commit

---
+++ 5. Concurrent Programming/Thread1.cpp
@@ -0,0 +1,18 @@
+// Threads1
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void function_1()
+{
+    cout << "Beauty is only skin deep" << endl;
+}
+
+int main()
+{
+    function_1();
+
+    return 0;
+}
