862588d00e1cb3f6f9b952a4ac6e721bbc49e494
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-25 18:09:49 +0530

    thread2.cpp added

---
+++ 5. Concurrent Programming/Thread2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void function_1()
+{
+    cout << "Beauty is only skin deep" << endl;
+}
+
+int main()
+{
+    std::thread t1(function_1);
+}
