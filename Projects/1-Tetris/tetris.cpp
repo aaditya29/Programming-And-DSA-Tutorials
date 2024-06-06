fc3a562a39ba7efd302f69eafbb6fe9b021e803a
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-06 16:27:49 +0530

    git commit "creating assets"

---
+++ Projects/1-Tetris/tetris.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+wstring tetromino[7]; // tetromino is a geometric shape composed of four squares, connected orthogonally
+
+int main()
+{
+    // Creating assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+}
