f607d7a8ccaad545e2f9be450ae41fdb1f0cae97
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-07 10:28:23 +0530

    added tetromino

--- Projects/1-Tetris/tetris.cpp
+++ Projects/1-Tetris/tetris.cpp
@@ -7,8 +7,38 @@ wstring tetromino[7]; // tetromino is a geometric shape composed of four squares
 int main()
 {
     // Creating assets
+    tetromino[0].append(L"..X."); //. to express empty space and X to represent part of the shape
     tetromino[0].append(L"..X.");
     tetromino[0].append(L"..X.");
     tetromino[0].append(L"..X.");
-    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"..X.");
+
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L"..X.");
+
+    tetromino[3].append(L"..X.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".X..");
+    tetromino[3].append(L"..X.");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L".X..");
+    tetromino[4].append(L"..X.");
+
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L".X..");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"..X.");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L"..X.");
 }
