ec754cafb7ca1fc83dc9b7824918c838961b8b82
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-07 11:56:41 +0530

    rotate function added

--- Projects/1-Tetris/tetris.cpp
+++ Projects/1-Tetris/tetris.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 wstring tetromino[7]; // tetromino is a geometric shape composed of four squares, connected orthogonally
 
+// making rotation function
+int Rotate(int px, int py, int r)
+{
+
+    switch (r % 4)
+    {
+    case 0:
+        return py * 4 + px; // roating 0 degrees
+    case 1:
+        return 12 + py - (px * 4); // rotating 90 degrees
+    case 2:
+        return 15 - (py * 4) - px; // rotating 180 degrees
+    case 3:
+        return 3 - py + (px * 4); // rotating 270 degrees
+    }
+}
+
 int main()
 {
     // Creating assets
